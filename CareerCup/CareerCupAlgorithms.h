1ebeeaa7e208055dc736a046dada715641332720
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2016-10-29 00:20:37 +0530

    Added problem statement

--- CareerCup/CareerCupAlgorithms.h
+++ CareerCup/CareerCupAlgorithms.h
@@ -21,6 +21,20 @@
 + (NSDictionary<NSNumber *, MapNode *> *)firstNonMatchingLeafPairFromtree:(Map *)treeOne treeTwo:(Map *)treeTwo;
 
 /// Time Complexity: O(n) and Space Complexity: O(n) where n are the no of intervals
+/*
+  There's a room with a TV and people are coming in and out to watch it. The TV is on only when there's at least a person in the room.
+  For each person that comes in, we record the start and end time. We want to know for how long the TV has been on. In other words:
+  Given a list of arrays of time intervals, write a function that calculates the total amount of time covered by the intervals.
+  For example:
+ 
+  input = [(1,4), (2,3)]
+  > 3
+  input = [(4,6), (1,2)]
+  > 3
+  input = [(1,4), (6,8), (2,4), (7,9), (10, 15)]
+  > 11
+
+ */
 + (NSUInteger)largestOverlappingLengthAmongstTheGivenIntervals:(NSArray<NSArray<NSNumber *> *> *)arrayOfIntervals;
 
 @end
