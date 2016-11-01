853af07bf8621eb2ce8b452b056ccc99bd7561cc
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2016-11-01 18:45:58 +0530

    Trie commit

--- Trie/PNTrie.h
+++ Trie/PNTrie.h
@@ -8,6 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
+@interface PNTrieNode : NSObject
+
+NS_ASSUME_NONNULL_BEGIN
+
+@property (strong, readonly, nonatomic) NSDictionary<NSString *, PNTrieNode *> *childrens;
+@property (assign, nonatomic) BOOL isEndOfObject;
+
+- (instancetype)init;
+- (void)insertChildKey:(NSString *)key;
+
+NS_ASSUME_NONNULL_END
+
+@end
+
 @interface PNTrie : NSObject
+
+NS_ASSUME_NONNULL_BEGIN
+
+@property (strong, readonly, nonatomic) PNTrieNode *root;
+
+- (instancetype)init;
+- (instancetype)initWithRoot:(NSString *)root;
+
+- (void)insertObject:(NSString *)type;
+- (NSArray<NSString *> *)prefixSearch:(NSString *)type;
+- (BOOL)containsObject:(NSString *)type;
+//- (BOOL)deleteObject:(NSString *)type;
+
+NS_ASSUME_NONNULL_END
 
 @end
--- Trie/PNTrie.m
+++ Trie/PNTrie.m
@@ -7,7 +7,194 @@
 //
 
 #import "PNTrie.h"
+#import "Stack.h"
+
+@interface PNTrieNode()
+
+@property (strong, nonatomic) NSMutableDictionary<NSString *, PNTrieNode *> *mutChildrens;
+
+@end
+
+@implementation PNTrieNode
+
+-(instancetype)init {
+    self = [super init];
+    if(self) {
+        _mutChildrens = @{}.mutableCopy;
+        _isEndOfObject = NO;
+    }
+    return self;
+}
+
+- (void)insertChildKey:(NSString *)key {
+    self.mutChildrens[key] = [[PNTrieNode alloc] init];
+}
+
+- (NSDictionary<NSString *,PNTrieNode *> *)childrens {
+    
+    if(!self.mutChildrens) {
+        return nil;
+    }
+    return [NSDictionary dictionaryWithDictionary:self.mutChildrens];
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"childerens: %@, endOfWord: %d", self.mutChildrens, self.isEndOfObject];
+}
+
+@end
+
+@interface PNTrie()
+
+@property (strong, nonatomic) PNTrieNode *root;
+
+@end
+
 
 @implementation PNTrie
+
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        _root = [[PNTrieNode alloc] init];
+    }
+    return self;
+}
+
+- (instancetype)initWithRoot:(NSString *)val {
+    
+    self = [self init];
+    [_root insertChildKey:val];
+    return self;
+}
+
+- (void)insertObject:(NSString *)type {
+    
+    [self insertObject:type withRoot:self.root];
+    
+}
+
+- (void)insertObject:(NSString *)key withRoot:(PNTrieNode *)node {
+    
+    NSString *queryKey = [key substringToIndex:1];
+    NSString *substring = [key substringFromIndex:1];
+    
+    if(!node.childrens[queryKey]) {
+        [node insertChildKey:queryKey];
+    }
+
+    node.childrens[queryKey].isEndOfObject = substring.length == 0;
+    
+    if(substring.length > 0) {
+        [self insertObject:substring withRoot:node.childrens[queryKey]];
+    }
+    else {
+        node.childrens[queryKey].isEndOfObject = YES;
+    }
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"DICT : %@", self.root];
+}
+
+- (NSArray<NSString *> *)prefixSearch:(NSString *)type {
+    
+    NSMutableArray<NSString *> *mutArray = @[].mutableCopy;
+    NSArray<NSString *> *tempArray = @[];
+    
+    PNTrieNode *node = self.root;
+    NSString *temp = nil;
+    NSUInteger i = 0;
+    for (i = 0; i < type.length; ++i) {
+        temp =  [NSString stringWithFormat:@"%C", [type characterAtIndex:i]];
+        if(node.childrens[temp]) {
+            node = node.childrens[temp];
+        }
+        else {
+            break;
+        }
+    }
+    
+    if(i < type.length) {
+        return nil;
+    }
+ 
+    tempArray = [self DFSFromeNode:node];
+    
+    if(tempArray.count == 0 || node.isEndOfObject) {
+        [mutArray addObject:type];
+    }
+    
+    for (NSString *str in tempArray) {
+        temp = [NSString stringWithFormat:@"%@%@", type, str];
+        [mutArray addObject:temp.copy];
+    }
+    
+    return [NSArray arrayWithArray:mutArray];
+}
+
+- (NSArray<NSString *> *)DFSFromeNode:(PNTrieNode *)node {
+    
+    if ([node.childrens allKeys].count <= 0) {
+        return @[];
+    }
+    
+    NSArray<NSString *> *dictKeys = node.childrens.allKeys;
+    NSMutableArray<NSString *> *mutArray = @[].mutableCopy;
+    NSString *tempStr = nil;
+    NSArray *arr = nil;
+    
+    for (NSString *keyString in dictKeys) {
+        arr = [self DFSFromeNode:node.childrens[keyString]];
+        if(arr.count == 0) {
+            [mutArray addObject:keyString];
+            continue;
+        }
+        for (NSString *str in arr) {
+            tempStr = [NSString stringWithFormat:@"%@%@", keyString, str];
+            [mutArray addObject:tempStr.copy];
+        }
+    }
+    
+    return [NSArray arrayWithArray:mutArray];
+}
+
+- (BOOL)containsObject:(NSString *)type {
+    
+    PNTrieNode *node = self.root;
+    NSString *temp = nil;
+    NSUInteger i = 0;
+    
+    for (i = 0; i < type.length; ++i) {
+        temp =  [NSString stringWithFormat:@"%C", [type characterAtIndex:i]];
+        if(node.childrens[temp]) {
+            node = node.childrens[temp];
+        }
+        else {
+            return NO;
+        }
+    }
+    return node.isEndOfObject;
+}
+
+//- (BOOL)deleteObject:(NSString *)type {
+//    if (![self containsObject:type]) {
+//        return NO;
+//    }
+//    PNTrieNode *node = self.root;
+//    NSString *temp = nil;
+//    NSUInteger i = 0;
+//    
+//    for (i = 0; i < type.length; ++i) {
+//        temp =  [NSString stringWithFormat:@"%C", [type characterAtIndex:i]];
+//        if(node.childrens[temp]) {
+//            node = node.childrens[temp];
+//        }
+//        else {
+//            return NO;
+//        }
+//    }
+//}
 
 @end
