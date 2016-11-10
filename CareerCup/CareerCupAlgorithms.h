22618432312f075732f2ba3dc47bb3e72b5ddad3
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2016-11-10 22:51:14 +0530

    Added a question

--- CareerCup/CareerCupAlgorithms.h
+++ CareerCup/CareerCupAlgorithms.h
@@ -88,4 +88,15 @@
 
 + (void)distanceFromGuardsAtOpenPlacesInMat:(NSMutableArray<NSMutableArray<NSString *> *> *)mat;
 
+
+/*
+ Task schedule: given a sequence of task like A B C(means 3 different tasks), and a coldtime, which means you need to wait for that much time to start next [same] task. Now----
+ 
+ Input: string, n
+ Output: the best task-finishing sequence.
+ 
+ eg. input: AAABBB, 2
+ Output: AB_AB_AB
+ ( "_" represents do nothing and wait)
+*/
 @end
